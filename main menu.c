af313c079998c70b900359e53965fd6c65454c40
Aashutosh Rathi <aashutoshrathi@gmail.com>
2016-10-25 17:07:41 +0530

    Add files via upload
    25 -oct file 2 main menu

--- main menu.c
+++ main menu.c
@@ -9,6 +9,63 @@
 #define YELLOW  "\033[33m"      /* Yellow */
 
 
+void changepass(char pass[])
+{
+    printf("\n\t\tEnter Your Previous Password :-");
+    char ch;
+    int i;
+  for(i=0;i<8;i++)
+ {
+  ch = getch();
+  pass[i] = ch;
+  ch = '*';
+  printf("%c",ch);
+ }
+  pass[i]= '\0';
+
+
+    FILE *fp;
+    char password[8];
+    fp=fopen("adminpass.txt","r+");
+    while(fscanf(fp,"%s",password)== 1)
+    {
+      int z=strcmp(password,pass);
+    if (z!=0)
+    {
+        printf(RED "\n\n\t\t Wrong Password, Try Again !!\n" RESET);
+        changepass(pass);
+    }
+    else
+    {
+        system("cls");
+        printf(GREEN "\n\n\t\tCorrect Password ...\n" RESET);
+        printf("\n\t\tEnter Your New Password :");
+        fprintf("%s",password);
+        char rdef[8];
+        printf("\n\t\t Enter Again :  ");
+        scanf("%s",rdef);
+        if(strcmp(password,rdef)==0)
+        {
+            printf("\n\t\tPassword Changed Successfully\n");
+            printf("\n\t\tPress 1 To Go Back to Main Menu\n");
+            int inp;
+            printf("\n\t\t");
+            scanf("%d",&inp);
+            if(inp==1)
+            {
+                main();
+            }
+            else
+            {
+                printf("\n\t\tPasswords do not match\n");
+                changepass(pass);
+            }
+        }
+    }
+    }
+}
+
+
 void passmenu()
 {
   int p;
@@ -88,10 +145,8 @@ switch(am)
 {
 case 1:
     {   system("cls");
-        printf("\n\t\tEnter Your Previous Password :- ");
         char prevpass[8];
-        passchecka(prevpass);
-
+        changepass(prevpass);
         break;
     }
 case 2:
