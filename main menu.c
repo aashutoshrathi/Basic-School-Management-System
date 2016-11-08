19bf6e856755b4eaf478639d88b9a95f1bf2250f
Aashutosh Rathi <aashutoshrathi@gmail.com>
2016-11-08 09:44:29 +0530

    update main menu.c

--- main menu.c
+++ main menu.c
@@ -2,14 +2,12 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
-#include <dos.h>
 
 #define RESET   "\033[0m"
 #define BLUE    "\033[34m"      /* Blue */
 #define RED     "\033[31m"      /* Red */
 #define GREEN   "\033[32m"      /* Green */
 #define YELLOW  "\033[33m"      /* Yellow */
-#define gap printf("\n\t\t");
 
 void preventry()
 {
@@ -22,6 +20,19 @@ void preventry()
         printf("%s\n",ch);
     }
     fclose(fp);
+}
+
+void stumenu()
+{
+    int m;
+    printf("\n\t\t\t\t ***** Welcome to Student MENU ***** \n");
+    printf("\n\n \t\t 1.Show Your Profile");
+    printf("\n\t\t 2.Past Performance");
+    printf("\n\t\t 3.Time Table");
+    printf("\n\n\t\t Your Choice :- ");
+    scanf("%d",&m);
+
+
 }
 
 void loginmenustu()
@@ -60,8 +71,429 @@ void loginmenustu()
             scanf("%s",passstu);
             if(strcmp(passstu,temp)==0)
             {
-                printf(GREEN "\n\t\t Correct Password" RESET);
-                //studentmenu();
+                system("cls");
+                printf(GREEN "\n\t\t Correct Password !! \n" RESET);
+                stumenu();
+            }
+
+}
+
+void studentpassmenu()
+{
+    int p;
+  printf("\n\t\t 1. Login Menu\n");
+  printf("\n\t\t 2. Go Back\n\t\t");
+  printf("\n\n\t\t Your Choice : ");
+  scanf("%d",&p);
+  switch(p)
+  {
+  case 1:
+   {
+        system("cls");
+       loginmenustu();
+   }
+  case 2:
+    {
+        main();
+        break;
+    }
+
+  }
+}
+
+void newstudent()
+{
+    FILE *ns;
+    char name[50];
+    int m;
+    char addr[100];
+    int srno;
+    char father[100];
+    char mother[100];
+    char stupass[8];
+    int clas;
+    ns=fopen("newstu.txt","a");
+    printf(YELLOW "\n\t\t\t ** New Student Entry Menu ** \n" RESET);
+
+    printf("\n\t\t SR No. : ");
+    scanf("%d",&srno);
+    fprintf(ns,"SR.:%d\n",srno);
+
+    printf("\n\t\t Name : ");
+    scanf("%s",name);
+    fprintf(ns,"Name:%s\n",name);
+
+    printf("\n\t\t Father's Name : ");
+    scanf("%s",father);
+    fprintf(ns,"Father's_Name:%s\n",father);
+
+    printf("\n\t\t Mother's Name : ");
+    scanf("%s",mother);
+    fprintf(ns,"Mother's_Name:%s\n",mother);
+
+    printf("\n\t\t Class : ");
+    scanf("%d",&clas);
+    fprintf(ns,"Class:%d\n",clas);
+
+    char contactstu[20];
+    printf("\n\t\t Contact : ");
+    scanf("%s",contactstu);
+    fprintf(ns,"Contact:%s\n",contactstu);
+
+    printf("\n\t\t Address : ");
+    scanf("%s",addr);
+    fprintf(ns,"Address:%s\n",addr);
+
+    int usrid = clas*100+srno;
+    fprintf(ns,"ID:%d\n",usrid);
+
+
+    printf(GREEN "\n\n\t\t New Student Entry Created Successfully \n " RESET);
+    printf("\n\t\t SR No.: %d \n\t\t Name : %s \n\t\t Father's Name : %s \n\t\t Mother's Name : %s  \n\t\t Class : %d \n\t\t Contact : %s \n\t\t Address : %s ",srno,name,father,mother,clas,contactstu,addr);
+
+
+    printf(YELLOW "\n\n\t\t Generated User ID :  "RESET);
+    printf("%d\n",usrid);
+
+    printf("\n\t\t Set a Password : ");
+    scanf("%s",stupass);
+    fprintf(ns,"%s\n",stupass);
+    fprintf(ns,"******************************************************************************* \n");
+
+    printf("\n\n \t\t 1.Create Another Entry");
+    printf("\n\t\t 2.Show Previous Entries");
+    printf("\n\t\t 3.Go Back to Main Menu");
+    printf("\n\n\t\t Your Choice :- ");
+    scanf("%d",&m);
+    switch(m)
+    {
+    case 1:
+        {
+            system("cls");
+            newstudent();
+            break;
+        }
+    case 2:
+        {
+            system("cls");
+            gap
+            preventry();
+            break;
+        }
+    case 3:
+        {
+            system("cls");
+            main();
+            break;
+        }
+    }
+
+}
+
+void changepass(char pass[])
+{
+    printf("\n\t\tEnter Your Previous Password :-");
+    char ch;
+    int i;
+  for(i=0;i<8;i++)
+ {
+  ch = getch();
+  pass[i] = ch;
+  ch = '*';
+  printf("%c",ch);
+ }
+  pass[i]= '\0';
+
+    char password[8];
+    FILE *fp;
+    fp=fopen("adminpass.txt","rw+");
+    while(fscanf(fp,"%s",password)== 1)
+    {
+      int z=strcmp(password,pass);
+    if (z!=0)
+    {
+        printf(RED "\n\n\t\t Wrong Password, Try Again !!\n" RESET);
+        changepass(pass);
+    }
+    else
+    {
+        system("cls");
+        printf(GREEN "\n\n\t\tCorrect Password ...\n" RESET);
+        printf("\n\t\tEnter Your New Password :");
+        scanf("%s",password);
+        fprintf(fp,"%s",password);
+        char rdef[9];
+        printf("\n\t\tEnter Again :  ");
+        scanf("%s",rdef);
+        if(strcmp(password,rdef)==0)
+        {
+            printf("\n\t\tPassword Changed Successfully\n");
+            printf("\n\t\tPress 1 To Go Back to Main Menu\n");
+            int inp;
+            printf("\n\t\t");
+            scanf("%d",&inp);
+            if(inp==1)
+            {
+                system("cls");
+                main();
+            }
+            else
+            {
+                printf("\n\t\tPasswords do not match\n");
+                changepass(pass);
+            }
+        }
+    }
+    }
+    fclose(fp);
+}
+
+void passmenu()
+{
+  int p;
+  printf("\n\t\t 1. Enter Password (8 Characters)\n");
+  printf("\n\t\t 2. Go Back\n\t\t");
+  printf("\n\n\t\t Your Choice : ");
+  scanf("%d",&p);
+  switch(p)
+  {
+  case 1:
+  { char s[100];
+    printf("\n\t\t Enter Password : ");
+    passchecka(s);
+    break;
+  }
+  case 2:
+  {
+    system("cls");
+    main();
+    break;
+
+  }
+  default:
+  {
+      printf("\n\t\t Invalid Option .. Please Try again.\n\n");
+      passmenu();
+
+  }
+  }
+
+}
+
+void passchecka(char passa[])
+{
+    char ch;
+    int i;
+  for(i=0;i<8;i++)
+ {
+  ch = getch();
+  passa[i] = ch;
+  ch = '*';
+  printf("%c",ch);
+ }
+  passa[i]= '\0';
+
+    FILE *fp;
+    char password[8];
+    fp=fopen("adminpass.txt","r+");
+    while(fscanf(fp,"%s",password)== 1)
+    {
+    if (strcmp(password,passa)!=0)
+    {
+        system("cls");
+        printf(RED "\n\n\t\t Wrong Password, Try Again !!\n" RESET);
+        passmenu();
+    }
+    else
+    {
+        system("cls");
+        printf(GREEN "\n\n\t\tCorrect Password ...\n" RESET);
+        adminmenu();
+    }
+
+    }
+}
+
+void adminmenu()
+{
+printf("\n\t\t\t\t ***** Welcome to Admin MENU ***** \n");
+printf("\n\t\t 1. Change Password\n");
+printf("\t\t 2. Enter New Admission\n");
+printf("\t\t 3. Enter New Teacher Details\n");
+printf("\t\t 4. Print All Student Details\n");
+printf("\t\t 5. Go to Main Menu\n");
+printf("\n\t\t Press Ctrl+C to Exit any time\n");
+printf("\n\t\t Enter your Choice :-  ");
+int am;
+scanf("%d",&am);
+switch(am)
+{
+case 1:
+    {   system("cls");
+        char prevpass[8];
+        changepass(prevpass);
+        break;
+    }
+case 2:
+    {   system("cls");
+        printf("\n\t\t Enter A New Student Entry \n");
+        newstudent();
+        break;
+    }
+case 3:
+    {   system("cls");
+        printf("\n\t\t Appointed a New Teacher !, Enter Details \n ");
+        passmenu();
+        break;
+    }
+case 4:
+    {
+        system("cls");
+        preventry();
+        break;
+    }
+
+case 5:
+    {   system("cls");
+        main();
+        break;
+    }
+
+default:
+    {   system("cls");
+        printf("\n\t\t Invalid Option .. Please Try again.\n\n");
+        main();
+        break;
+    }
+
+}
+
+}
+
+int main()
+{
+printf("\n\t\t\t\t ***** Welcome to School Management System ***** \n");
+printf("\n\t\t 1. Admin Login\n");
+printf("\t\t 2. Teacher Login\n");
+printf("\t\t 3. Student Login\n");
+printf("\t\t 4. BETA Testing Feature\n");
+printf("\n\t\t Press Ctrl+C to Exit any time\n");
+printf("\n\t\t Enter your Choice :-  ");
+int c;
+scanf("%d",&c);
+switch(c)
+{
+case 1:
+    {   system("cls");
+        printf("\n\t\t You selected Admin Login \n ");
+        passmenu();
+        break;
+    }
+case 2:
+    {   system("cls");
+        printf("\n\t\t You selected Teacher Login \n");
+        passmenu();
+        break;
+    }
+case 3:
+    {
+        system("cls");
+        printf("\n\t\t You selected Student Login \n ");
+        loginmenustu();
+        break;
+    }
+case 4:
+    {
+        system("cls");
+        newstudent();
+    }
+default:
+    {
+        system("cls");
+        printf("\n\t\t Invalid Option .. Please Try again.\n\n");
+        main();
+        break;
+    }
+
+}
+return 0;
+}
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define RESET   "\033[0m"
+#define BLUE    "\033[34m"      /* Blue */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+
+void preventry()
+{
+    FILE *fp;
+    char ch[100];
+    fp=fopen("newstu.txt","r");
+    while(!feof(fp))
+    {
+        fscanf(fp,"%s",ch);
+        printf("%s\n",ch);
+    }
+    fclose(fp);
+}
+
+void stumenu()
+{
+    int m;
+    printf("\n\t\t\t\t ***** Welcome to Student MENU ***** \n");
+    printf("\n\n \t\t 1.Show Your Profile");
+    printf("\n\t\t 2.Past Performance");
+    printf("\n\t\t 3.Time Table");
+    printf("\n\n\t\t Your Choice :- ");
+    scanf("%d",&m);
+
+
+}
+
+void loginmenustu()
+{
+    loop:
+    printf("");
+    FILE *stulog;
+    char free[100]="ID:";
+    char userstu[100];
+    char passstu[100];
+    char temp[100];
+    printf("\n\t\t Enter Your User ID : ");
+    scanf("%s",userstu);
+    strcat(free,userstu);
+    stulog=fopen("newstu.txt","r");
+    int skipper=0;
+    while(!feof(stulog))
+    {
+        fscanf(stulog,"%s",temp);
+        if(strcmp(temp,free) == 0)
+        {
+            fscanf(stulog,"%s",temp);
+            skipper=1;
+            break;
+        }
+
+    }
+            if(skipper==0)
+            {
+            system("cls");
+            printf("\n\t\t User ID Entered does not exist, Try again or contact Administrator");
+            goto loop;
+            }
+            printf(GREEN "\n\t\t User ID Found\n" RESET);
+            printf("\n\t\t Enter Your Password : ");
+            scanf("%s",passstu);
+            if(strcmp(passstu,temp)==0)
+            {
+                system("cls");
+                printf(GREEN "\n\t\t Correct Password !! \n" RESET);
+                stumenu();
             }
 
 }
