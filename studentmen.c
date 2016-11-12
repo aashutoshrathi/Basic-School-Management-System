2b59d729e39ee55af681ae7b83b112fddfa351ca
Aashutosh Rathi <aashutoshrathi@gmail.com>
2016-11-13 04:49:14 +0530

    studentmenu.c

--- studentmen.c
+++ studentmen.c
@@ -1,50 +1,26 @@
-void loginmenustu()
+void stumenu()
 {
-    FILE *stulog;
-    char userstu[100];
-    char passstu[100];
-    char temp[100];
-    printf("\n\t\t Enter Your User ID : ");
-    scanf("%s",userstu);
-    stulog=fopen("newstu","r+");
+    int m;
+    printf("\n\t\t\t\t ***** Welcome to Student MENU ***** \n");
+    printf("\n\n \t\t 1.Show Your Profile");
+    printf("\n\t\t 2.Past Performance");
+    printf("\n\t\t 3.Time Table");
+    printf("\n\t\t 7.Exit");
 
-    while(fscanf(stulog, "%s", temp) != EOF)
+    printf("\n\n\t\t Your Choice :- ");
+    scanf("%d",&m);
+    switch(m)
     {
-        if(strcmp(temp,userstu) == 0)
-        {
-            printf("\n\t\t User ID Found");
-            printf("\n\t\t Enter Your Password : ");
-            scanf("%s",passstu);
-        }
-        else
-        {
-            system("cls");
-            printf("\n\t\t User ID Entered does not exist, Try again or contact Administrator");
-            loginmenustu();
-        }
-    }
-}
-
-
-void studentpassmenu()
-{
-    int p;
-  printf("\n\t\t 1. Login Menu\n");
-  printf("\n\t\t 2. Go Back\n\t\t");
-  printf("\n\n\t\t Your Choice : ");
-  scanf("%d",&p);
-  switch(p)
-  {
-  case 1:
-   {
-        system("cls");
-       loginmenustu();
-   }
-  case 2:
+    case 1:
     {
-        main();
-        break;
+
     }
 
-  }
+    case 7:
+    {
+        exit(0);
+    }
+
+    }
+
 }
