60fd86252db53fdc83cf3a77270312b5fa6c2cb1
Aashutosh Rathi <aashutoshrathi@gmail.com>
2016-11-14 02:10:23 +0530

    newstu.c
    Update Structure

--- newstu.c
+++ newstu.c
@@ -21,36 +21,27 @@ void newstudent()
 
     printf("\n\t\t SR No. : ");
     scanf("%d",&s.srno);
-    fprintf(ns,"SR.:%d\n",s.srno);
 
     printf("\n\t\t Name : ");
     scanf(" %[^\n]s",s.name);
-    fprintf(ns,"Name:%s\n",s.name);
 
     printf("\n\t\t Father's Name : ");
     scanf(" %[^\n]s",s.father);
-    fprintf(ns,"Father's_Name:%s\n",s.father);
 
     printf("\n\t\t Mother's Name : ");
     scanf(" %[^\n]s",s.mother);
-    fprintf(ns,"Mother's_Name:%s\n",s.mother);
 
     printf("\n\t\t Class : ");
     scanf("%d",&s.clas);
-    fprintf(ns,"Class:%d\n",s.clas);
-
 
     printf("\n\t\t Contact : ");
     scanf(" %[^\n]s",s.contactstu);
-    fprintf(ns,"Contact:%s\n",s.contactstu);
 
 
     printf("\n\t\t Address : ");
     scanf(" %[^\n]s",s.addr);
-    fprintf(ns,"Address:%s\n",s.addr);
 
     s.usrid = s.clas*100+s.srno;
-    fprintf(ns,"ID:%d\n",s.usrid);
 
 
     printf(GREEN "\n\n\t\t New Student Entry Created Successfully \n " RESET);
@@ -62,8 +53,9 @@ void newstudent()
 
     printf("\n\t\t Set a Password : ");
     scanf("%s",s.stupass);
-    fprintf(ns,"%s\n",s.stupass);
-    fprintf(ns,"*******************************************************************************************\n");
+
+    fseek(ns,0,SEEK_END);
+    fwrite(&s,sizeof(s),1,ns);
 
     printf("\n\n \t\t 1.Create Another Entry");
     printf("\n\t\t 2.Show Previous Entries");
